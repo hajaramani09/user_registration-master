664c9072440be8aca8fc8d1b8524342fc5f92059
hajaramani09 <hajar6amani@gmail.com>
2024-02-02 10:22:30 +0100

    comm

--- src/main/java/codingtechniques/WebServerFactory.java
+++ src/main/java/codingtechniques/WebServerFactory.java
@@ -8,6 +8,6 @@ public class WebServerFactory implements
 
     @Override
     public void customize(ConfigurableServletWebServerFactory factory) {
-        factory.setPort(8080);
+        factory.setPort(9001);
     }
 }
