09806ad947b954528caa922901c2e44375ae6541
Abdelilah-BH <bouhoudev18@gmail.com>
2024-02-02 11:23:42 +0100

    fix

--- src/main/java/codingtechniques/UserRegistrationApplication.java
+++ src/main/java/codingtechniques/UserRegistrationApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -17,15 +18,15 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @ComponentScan(basePackages = { "main.java.codingtechniques" })
 @SpringBootApplication(scanBasePackages = "main.java.codingtechniques", exclude = {
 		HibernateJpaAutoConfiguration.class })
-//@EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class })
-@EnableAutoConfiguration()
+@EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class })
+// @EnableAutoConfiguration()
 
 public class UserRegistrationApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(UserRegistrationApplication.class, args);
-		
+
 
 	}
 
